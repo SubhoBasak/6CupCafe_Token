4390cf61b2f17ad58c7667b5cd1143d69a2caa03
SubhoBasak <subhobasak50@gmail.com>
2022-06-29 11:22:22 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import "./App.css";
+
+const Card = ({ token }) => {
+  return (
+    <Col
+      xl="2"
+      lg="3"
+      md="6"
+      sm="1"
+      className="d-flex justify-content-center align-items-center text-danger py-3"
+    >
+      <div className="token-card d-flex justify-content-center align-items-center">
+        {token}
+      </div>
+    </Col>
+  );
+};
 
 function App() {
+  const [tokens, setTokens] = React.useState([]);
+
+  setInterval(() => window.location.reload(), 10000);
+
+  React.useEffect(() => {
+    fetch(process.env.REACT_APP_BASE_URL + "/token/open", {
+      method: "GET",
+    }).then((res) => {
+      if (res.status === 200) res.json().then((data) => setTokens(data));
+      else return alert("Something went wrong! Please try again.");
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="token-canvas">
+      <div className="nav-title-bar d-flex align-items-center">
+        <img src={require("./assets/images/logo-light.png")} alt="logo" />
+        <p className="fs-4 text-light my-0 ms-3 fw-light">6 Cups Cafe</p>
+      </div>
+      <Row className="p-3 w-100">
+        {tokens.map((data, index) => (
+          <Card key={index} token={data.token} />
+        ))}
+      </Row>
     </div>
   );
 }
