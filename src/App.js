855f00db4f90cc0c040f5ca0e6c18ccdb9683899
SubhoBasak <subhobasak50@gmail.com>
2022-07-12 22:06:21 +0530

    change style

--- src/App.js
+++ src/App.js
@@ -5,45 +5,63 @@ import "./App.css";
 const Card = ({ token }) => {
   return (
     <Col
-      xl="2"
-      lg="3"
+      xl="3"
+      lg="4"
       md="6"
       sm="1"
-      className="d-flex justify-content-center align-items-center text-danger py-3"
+      className="d-flex justify-content-center align-items-center py-3"
     >
-      <div className="token-card d-flex justify-content-center align-items-center">
-        {token}
+      <div className="token-card d-flex flex-column justify-content-center align-items-center">
+        <div className="w-100 d-flex justify-content-start py-0 px-5 my-0">
+          <span className="fs-5 text-light">Token -</span>
+        </div>
+        <p className="my-0 py-0">{token}</p>
+        <div className="my-0 py-0 w-100 d-flex justify-content-end">
+          <img src={require("./assets/images/logo.png")} alt="logo" />
+        </div>
       </div>
     </Col>
   );
 };
 
 function App() {
-  const [tokens, setTokens] = React.useState([]);
+  const [tokens, setTokens] = React.useState([
+    { token: 1 },
+    { token: 2 },
+    { token: 3 },
+    { token: 4 },
+    { token: 5 },
+    { token: 6 },
+    { token: 7 },
+    { token: 8 },
+    { token: 9 },
+  ]);
 
   setInterval(() => window.location.reload(), 10000);
 
   React.useEffect(() => {
-    fetch(process.env.REACT_APP_BASE_URL + "/token/open", {
-      method: "GET",
-    }).then((res) => {
-      if (res.status === 200) res.json().then((data) => setTokens(data));
-      else return alert("Something went wrong! Please try again.");
-    });
+    // fetch(process.env.REACT_APP_BASE_URL + "/token/open", {
+    //   method: "GET",
+    // }).then((res) => {
+    //   if (res.status === 200) res.json().then((data) => setTokens(data));
+    //   else return alert("Something went wrong! Please try again.");
+    // });
   }, []);
 
   return (
-    <div className="token-canvas">
-      <div className="nav-title-bar d-flex align-items-center">
+    <>
+      {/* <div className="nav-title-bar d-flex align-items-center">
         <img src={require("./assets/images/logo-light.png")} alt="logo" />
         <p className="fs-4 text-light my-0 ms-3 fw-light">6 Cups Cafe</p>
+      </div> */}
+      <div className="token-canvas">
+        <Row className="p-3 w-100">
+          {tokens.map((data, index) => (
+            <Card key={index} token={data.token.toString().padStart(3, "0")} />
+          ))}
+        </Row>
       </div>
-      <Row className="p-3 w-100">
-        {tokens.map((data, index) => (
-          <Card key={index} token={data.token} />
-        ))}
-      </Row>
-    </div>
+    </>
   );
 }
 
